4e3f3cda8d4435d67180b1e14a807e7a5b9512f9
Leopold Roos <leopold@leopold.se>
2022-10-24 10:11:41 +0200

    More AWS intital setup

--- lib/jaws-stack.ts
+++ lib/jaws-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class JawsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,5 +13,18 @@ export class JawsStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'JawsQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    new ec2.Vpc(this, 'mainVPC', {
+      // This is where you can define how many AZs you want to use
+      maxAzs: 2,
+      // This is where you can define the subnet configuration per AZ
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'public-subnet',
+          subnetType: ec2.SubnetType.PUBLIC,
+        }
+      ]
+    });
   }
 }
