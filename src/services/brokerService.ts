b33ea2d1a0881bd10e73524b197175399c50ddb0
Gustaf Liljegren <gustafliljegren@MacBook-Pro-som-tillhor-Gustaf-2.local>
2022-11-01 14:46:54 +0100

    remove hostUrl because Next is king (kept util function as isDev)

--- src/services/brokerService.ts
+++ src/services/brokerService.ts
@@ -1,24 +1,22 @@
 import fetch from "node-fetch";
 import { Order } from "../components/organisms/OrderList";
-import { convertResult, hostUrl } from "../util";
-
-const baseUrl = hostUrl()
+import { convertResult } from "../util";
 
 export const brokerService = async () => {
-  const resp = await fetch(`${baseUrl}/api/broker/place-order`);
+  const resp = await fetch(`/api/broker/place-order`);
   const data = await convertResult(resp);
 };
 
 export const handlePostOrder = async (ticker: string) => {
   const resp = await fetch(
-    `${baseUrl}/api/broker/place-order/?ticker=${ticker}`,
+    `/api/broker/place-order/?ticker=${ticker}`,
     { method: "POST" },
   );
   const data = await convertResult(resp);
 };
 
 export const handleGetTrades = async () => {
-  const resp = await fetch(`${baseUrl}/api/broker/get-orders`);
+  const resp = await fetch(`/api/broker/get-orders`);
   const data = await convertResult(resp);
   const response = data.orders.map((order: Order) => {
     const { symbol, status, notional, created_at, filled_at, side } = order;
--- src/util/index.ts
+++ src/util/index.ts
@@ -1,2 +1,2 @@
 export * from './convertResult'
-export * from './hostUrl'
+export * from './isDev'
