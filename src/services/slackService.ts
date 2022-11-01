7405a915b943475cb63a0a3f9c290600a2b02536
Anna Gustavsson <annalovisaellen@gmail.com>
2022-11-01 11:36:38 +0100

    add image in slacj message and link to jaws daily run

--- src/services/slackService.ts
+++ src/services/slackService.ts
@@ -3,10 +3,28 @@ import { BodyInit } from "node-fetch";
 import { convertResult } from "../util/convertResult";
 
 export const postSlackMessage = async () => {
-  const body: BodyInit = JSON.stringify({ text: "From Jaws app!" });
+  const scarySharkImgUrl =
+    "https://static.nationalgeographic.co.uk/files/styles/image_3200/public/shark-scary-animals.jpg?w=1600";
+
+  const body: BodyInit = JSON.stringify({
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "Time for your daily picks!! \n <https://jaws-sharkster.netlify.app/daily-run|Link to daily run>",
+        },
+        accessory: {
+          type: "image",
+          image_url: scarySharkImgUrl,
+          alt_text: "scary shark",
+        },
+      },
+    ],
+  });
 
   const resp = await fetch(
-    `https://hooks.slack.com/services/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`,
+    `https://hooks.slack.com/services/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`,
     { method: "POST", body },
   );
 
