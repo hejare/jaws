da16864654063e7dea680718b569bdb498acae39
Anna Gustavsson <annalovisaellen@gmail.com>
2022-11-11 14:08:11 +0100

    fix css props

--- src/components/molecules/ModalDialog.tsx
+++ src/components/molecules/ModalDialog.tsx
@@ -47,7 +47,7 @@ const CancelButtonContainer = styled.div`
 `;
 
 const style = {
-  position: "absolute" as "absolute",
+  position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
