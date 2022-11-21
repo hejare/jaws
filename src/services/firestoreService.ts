ca66e209872690dc8d1deb6543cc8b0178341a9a
Leopold Roos <leopold@leopold.se>
2022-11-21 10:34:19 +0100

    Remove a console.log

--- src/services/firestoreService.ts
+++ src/services/firestoreService.ts
@@ -7,7 +7,6 @@ import {
 import { getFirestore } from "firebase-admin/firestore";
 import { certJson } from "./firebaseCert";
 
-console.log("certJson:", certJson);
 if (!getApps().length) {
   initializeApp({
     credential: cert(<ServiceAccount>certJson),
