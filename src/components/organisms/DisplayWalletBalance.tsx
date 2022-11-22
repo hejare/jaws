684bbd6e34a2a09edc9128db09265cdfa0f03ff4
Anna Gustavsson <annalovisaellen@gmail.com>
2022-11-22 16:08:07 +0100

    round wallet balance value

--- src/components/organisms/DisplayWalletBalance.tsx
+++ src/components/organisms/DisplayWalletBalance.tsx
@@ -5,10 +5,14 @@ import TextDisplay from "../molecules/TextDisplay";
 const DisplayWalletBalance = () => {
   const [walletBalance, setWalletBalance] = useState<string>("");
 
+  const handleSetBalance = (balance: string) => {
+    setWalletBalance(parseFloat(balance).toFixed(2).toString());
+  };
+
   useEffect(() => {
     const fetchData = async () => {
       const result = await getWalletBalance();
-      result && setWalletBalance(result);
+      result && handleSetBalance(result);
     };
     void fetchData();
   }, []);
