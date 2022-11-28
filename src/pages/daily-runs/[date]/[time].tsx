b3176bb4c0ccf518fa3a2c1ee044112933f8da93
Anna Gustavsson <annalovisaellen@gmail.com>
2022-11-28 14:55:25 +0100

    to not display button if daily run is in the past

--- src/pages/daily-runs/[date]/[time].tsx
+++ src/pages/daily-runs/[date]/[time].tsx
@@ -85,18 +85,18 @@ const DailyRun: NextPage = () => {
       .catch(console.error);
   }, [date, time]);
 
-  const isDateToday = (date: string) => {
-    const currentDate = new Date().setHours(0, 0, 0);
-    const compareDate = new Date(
-      `${date.substring(0, 4)}-${date.substring(4, 6)}-${date.substring(6, 8)}`,
-    ).setHours(0, 0, 0);
+  // const isDateToday = (date: string) => {
+  //   const currentDate = new Date().setHours(0, 0, 0);
+  //   const compareDate = new Date(
+  //     `${date.substring(0, 4)}-${date.substring(4, 6)}-${date.substring(6, 8)}`,
+  //   ).setHours(0, 0, 0);
 
-    if (currentDate !== compareDate) {
-      return false;
-    }
+  //   if (currentDate !== compareDate) {
+  //     return false;
+  //   }
 
-    return true;
-  };
+  //   return true;
+  // };
 
   return (
     <PageContainer>
