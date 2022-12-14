04d684e885a87cb018b6a2f9f32e7cdb1b3f32e4
Anna Gustavsson <annalovisaellen@gmail.com>
2022-12-14 10:28:25 +0100

    get moving average function

--- src/services/polygonService.ts
+++ src/services/polygonService.ts
@@ -4,7 +4,14 @@ import {
   polygonClient,
   restClient,
 } from "@polygon.io/client-js";
+import fetch from "node-fetch";
 import { getTodayWithDashes } from "../lib/helpers";
+import { handleResult } from "../util";
+
+export enum MADays {
+  TEN = 10,
+  FIFTY = 50,
+}
 
 const { POLYGON_KEY = "[NOT_DEFINED_IN_ENV]" } = process.env;
 
@@ -36,4 +43,12 @@ export const getLastTradesMedianPrice = async (symbol: string) => {
       // even length:
       return prices[prices.length / 2 - 1];
     });
+};
+//https://polygon.io/docs/stocks/get_v1_indicators_sma__stockticker#:~:text=1605195918306274000%0A%20%20%20%20%7D%0A%7D-,Simple%20Moving%20Average%20(SMA),-GET
+export const getMovingAverage = async (symbol: string, timeRange: MADays) => {
+  const url = `https://api.polygon.io/v1/indicators/sma/${symbol}?timespan=day&adjusted=true&window=${timeRange}&series_type=close&order=desc&limit=1&apiKey=${POLYGON_KEY}`;
+  const response = await fetch(url);
+  const data = await handleResult(response);
+  return data.results.values[0].value;
+  // polygonClient doesn't have support for this endpoint?
 };
