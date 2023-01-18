f05dc0faa23db38fc2658b02dfb2b9ccf394c7bf
Daniel Molin <daniel@hejare.se>
2023-01-18 13:05:42 +0100

    api/broker/orders: cleanup

--- src/components/molecules/PlaceOrderButton.tsx
+++ src/components/molecules/PlaceOrderButton.tsx
@@ -92,7 +92,7 @@ const PlaceOrderButton = ({
             status: TRADE_STATUS.READY,
             side: TRADE_SIDE.BUY,
           });
-          /* Note: rn this will not place an actual order. See /api/broker/orders */
+
           void handleBuyOrder(ticker, buyPrice, quantity, breakoutRef);
           typeof onClick === "function" && onClick();
         }}
