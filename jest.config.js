55a5325b9cb0beaddc4df71c64ff4d38e77e4537
Daniel Molin <dkd333@gmail.com>
2023-01-23 16:28:21 +0100

    jest: add coverage

--- jest.config.js
+++ jest.config.js
@@ -14,4 +14,13 @@ module.exports = {
   moduleNameMapper: pathsToModuleNameMapper(
     compilerOptions.paths /*, { prefix: '<rootDir>/' } */,
   ),
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
 };
