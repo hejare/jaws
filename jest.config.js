6eab5befcc90c74df59f662e29287d2e85242345
Daniel Molin <dkd333@gmail.com>
2023-01-23 16:25:30 +0100

    jest: fix support for ts paths

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,17 @@
+const { pathsToModuleNameMapper } = require("ts-jest");
+
+// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
+// which contains the path mapping (ie the `compilerOptions.paths` option):
+const { compilerOptions } = require("./tsconfig");
+
 module.exports = {
-  testEnvironment: "node",
   roots: ["<rootDir>/test", "<rootDir>/src/"],
-  testMatch: ["**/*.test.ts"],
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
+  preset: "ts-jest",
+  modulePaths: [compilerOptions.baseUrl], // <-- This will be set to 'baseUrl' value
+  moduleNameMapper: pathsToModuleNameMapper(
+    compilerOptions.paths /*, { prefix: '<rootDir>/' } */,
+  ),
 };
