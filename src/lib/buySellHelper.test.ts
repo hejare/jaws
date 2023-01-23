ae77ae5ace3f6bbbdf6858d8c9189c83747e01d1
Daniel Molin <dkd333@gmail.com>
2023-01-23 15:14:34 +0100

    buySellHelper: expose config

--- src/lib/buySellHelper.test.ts
+++ src/lib/buySellHelper.test.ts
@@ -12,4 +12,24 @@ describe("buySellHelper", () => {
       }).getStopLossLimit(100),
     ).toBe(2);
   });
+
+  it("exposes a copy of the config", () => {
+    const helpers1 = getBuySellHelpers();
+
+    const defaultConfig = {
+      STOP_LOSS_1_PORTFOLIO_PERCENTAGE: 0.005,
+      STOP_LOSS_2_ENABLED: true,
+      TAKE_PARTIAL_PROFIT_INCREASE_PERCENTAGE: 0.1,
+      TAKE_PARTIAL_PROFIT_SELL_PERCENTAGE: 0.5,
+      BUY_ORDER_TIME_IN_FORCE: "day",
+      MOVING_AVERAGE_DAY_RANGE: 10,
+    };
+
+    expect(helpers1.config).toStrictEqual(defaultConfig);
+
+    const helpers2 = getBuySellHelpers({
+      STOP_LOSS_1_PORTFOLIO_PERCENTAGE: 0.23,
+    });
+    expect(helpers2.config.STOP_LOSS_1_PORTFOLIO_PERCENTAGE).toBe(0.23);
+  });
 });
--- src/lib/buySellHelper.ts
+++ src/lib/buySellHelper.ts
@@ -6,6 +6,7 @@ export interface BuySellConstants {
   TAKE_PARTIAL_PROFIT_INCREASE_PERCENTAGE: number;
   TAKE_PARTIAL_PROFIT_SELL_PERCENTAGE: number;
   BUY_ORDER_TIME_IN_FORCE: OrderTimeInForce;
+  MOVING_AVERAGE_DAY_RANGE: number;
 }
 
 const DEFAULT_CONFIG: BuySellConstants = {
@@ -14,6 +15,7 @@ const DEFAULT_CONFIG: BuySellConstants = {
   TAKE_PARTIAL_PROFIT_INCREASE_PERCENTAGE: 0.1,
   TAKE_PARTIAL_PROFIT_SELL_PERCENTAGE: 0.5,
   BUY_ORDER_TIME_IN_FORCE: "day",
+  MOVING_AVERAGE_DAY_RANGE: 10,
 };
 
 export const getBuySellHelpers = (config?: Partial<BuySellConstants>) => {
@@ -22,5 +24,9 @@ export const getBuySellHelpers = (config?: Partial<BuySellConstants>) => {
   return {
     getStopLossLimit: (totalAssets: number): number =>
       totalAssets * _config.STOP_LOSS_1_PORTFOLIO_PERCENTAGE,
+
+    get config() {
+      return { ..._config } as const;
+    },
   };
 };
