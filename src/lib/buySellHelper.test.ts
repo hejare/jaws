a2e968bdf3c324e4333c8e487ea5f80fc5d84539
Daniel Molin <dkd333@gmail.com>
2023-01-24 10:29:48 +0100

    Add test for only take partial profit once

--- src/lib/buySellHelper.test.ts
+++ src/lib/buySellHelper.test.ts
@@ -1,4 +1,4 @@
-import { TRADE_STATUS } from "@jaws/db/tradesMeta";
+import { TradesDataType, TRADE_SIDE, TRADE_STATUS } from "@jaws/db/tradesMeta";
 import { getBuySellHelpers } from "./buySellHelper";
 
 describe("buySellHelper", () => {
@@ -42,11 +42,15 @@ describe("buySellHelper", () => {
     const stopLossMaxAmount = helpers1.getStopLossMaxAmount(10000);
 
     it("should only return some statuses on day 1", () => {
-      const todayTrade: any = {
+      const todayTrade: TradesDataType = {
         price: 20,
         quantity: 15,
         breakoutRef: "BREAKOUT_REF",
         created: Date.now(),
+        side: TRADE_SIDE.BUY,
+        status: TRADE_STATUS.FILLED,
+        ticker: "GOOG",
+        alpacaOrderId: "ALPACA_ORDER_ID",
       };
 
       const tradeTakePartialProfit = helpers1.determineTradeStatus({
@@ -87,18 +91,22 @@ describe("buySellHelper", () => {
     });
 
     it("should check for all statuses on day >1", () => {
-      const yesterdayTrade: any = {
+      const yesterdayTrade: TradesDataType = {
         price: 20,
         quantity: 15,
         breakoutRef: "BREAKOUT_REF",
         created: Date.now() - 60 * 60 * 24 * 1000,
+        side: TRADE_SIDE.BUY,
+        status: TRADE_STATUS.FILLED,
+        ticker: "GOOG",
+        alpacaOrderId: "ALPACA_ORDER_ID",
       };
 
       const tradeTakePartialProfit = helpers1.determineTradeStatus({
         trade: yesterdayTrade,
         lastTradePrice: 25, // up more than 10%
         movingAvg: 23,
-        stopLossMaxAmount: stopLossMaxAmount,
+        stopLossMaxAmount,
       });
 
       expect(tradeTakePartialProfit).toBe(TRADE_STATUS.TAKE_PARTIAL_PROFIT);
@@ -107,7 +115,7 @@ describe("buySellHelper", () => {
         trade: yesterdayTrade,
         lastTradePrice: 15,
         movingAvg: 23,
-        stopLossMaxAmount: stopLossMaxAmount,
+        stopLossMaxAmount,
       });
 
       expect(tradeStopLoss1).toBe(TRADE_STATUS.STOP_LOSS_1);
@@ -116,7 +124,7 @@ describe("buySellHelper", () => {
         trade: yesterdayTrade,
         lastTradePrice: 19.5, // dropped below entry, should be STOP_LOSS_2
         movingAvg: 23,
-        stopLossMaxAmount: stopLossMaxAmount,
+        stopLossMaxAmount,
       });
 
       expect(tradeStopLoss2).toBe(TRADE_STATUS.STOP_LOSS_2);
@@ -125,10 +133,55 @@ describe("buySellHelper", () => {
         trade: yesterdayTrade,
         lastTradePrice: 20.5, // dropped below movingAvg, should be STOP_LOSS_3
         movingAvg: 20.7,
-        stopLossMaxAmount: stopLossMaxAmount,
+        stopLossMaxAmount,
       });
 
       expect(tradeStopLoss3).toBe(TRADE_STATUS.STOP_LOSS_3);
     });
+  });
+  it("should only take partial profit once", () => {
+    const helpers1 = getBuySellHelpers({
+      STOP_LOSS_1_PORTFOLIO_PERCENTAGE: 0.005,
+    });
+
+    const stopLossMaxAmount = helpers1.getStopLossMaxAmount(10000);
+
+    const yesterdayTrade: TradesDataType = {
+      price: 20,
+      quantity: 15,
+      breakoutRef: "BREAKOUT_REF",
+      created: Date.now() - 60 * 60 * 24 * 1000,
+      side: TRADE_SIDE.BUY,
+      status: TRADE_STATUS.FILLED,
+      ticker: "GOOG",
+      alpacaOrderId: "ALPACA_ORDER_ID",
+    };
+
+    const tradeTakePartialProfit = helpers1.determineTradeStatus({
+      trade: yesterdayTrade,
+      lastTradePrice: 25, // up more than 10%
+      movingAvg: 23,
+      stopLossMaxAmount,
+    });
+
+    expect(tradeTakePartialProfit).toBe(TRADE_STATUS.TAKE_PARTIAL_PROFIT);
+
+    const secondStatusCheck = helpers1.determineTradeStatus({
+      trade: { ...yesterdayTrade, status: TRADE_STATUS.TAKE_PARTIAL_PROFIT },
+      lastTradePrice: 25, // up more than 10%
+      movingAvg: 23,
+      stopLossMaxAmount,
+    });
+
+    expect(secondStatusCheck).toBe(undefined);
+
+    const tradeDroppedValue = helpers1.determineTradeStatus({
+      trade: { ...yesterdayTrade, status: TRADE_STATUS.TAKE_PARTIAL_PROFIT },
+      lastTradePrice: 21,
+      movingAvg: 23,
+      stopLossMaxAmount,
+    });
+
+    expect(tradeDroppedValue).toBe(TRADE_STATUS.STOP_LOSS_3);
   });
 });
