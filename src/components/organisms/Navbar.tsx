c97d9bfa198d413251bcf4e0fa837fb1295b4268
Daniel Molin <daniel@hejare.se>
2023-02-06 13:22:12 +0100

    NavBar: make links use a tag

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -1,15 +1,15 @@
-import Link from "next/link";
-import React, { useContext, useEffect, useState } from "react";
-import styled, { css } from "styled-components";
-import { getToday } from "@jaws/lib/helpers";
-import { Theme } from "@jaws/styles/themes";
-import { useRouter } from "next/router";
-import { User } from "@jaws/store/accountStore";
 import { signInWithGoogle } from "@jaws/auth/firestoreAuth";
-import Button from "../atoms/buttons/Button";
-import { setCookies } from "cookies-next";
-import { useStore } from "zustand";
+import { getToday } from "@jaws/lib/helpers";
 import { AccountContext } from "@jaws/store/accountContext";
+import { User } from "@jaws/store/accountStore";
+import { Theme } from "@jaws/styles/themes";
+import { setCookies } from "cookies-next";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useContext, useEffect, useState } from "react";
+import styled, { css } from "styled-components";
+import { useStore } from "zustand";
+import Button from "../atoms/buttons/Button";
 
 const NavBarContainer = styled.div`
   display: flex;
@@ -44,15 +44,17 @@ const LinksContainer = styled.div`
   gap: 15px;
 `;
 
-const NavBarItem = styled.div`
+const NavBarItem = styled.a`
   display: flex;
   align-items: center;
   justify-content: center;
   height: 100%;
   cursor: pointer;
+  text-decoration: none;
   :hover {
     color: ${({ theme }) => theme.palette.actionHover.text};
     transform: scale(1.05);
+    text-decoration: none;
   }
   ${({ active }: { active: boolean }) =>
     active &&
@@ -103,21 +105,15 @@ const Navbar = () => {
         <LogoContainer />
       </Link>
       <LinksContainer>
-        <Link href={`/daily-runs/${today}`}>
-          <NavBarItem
-            active={
-              pathName === "/daily-runs" ||
-              pathName === "/daily-runs/[date]" ||
-              pathName === "/daily-runs/[date]/[time]"
-            }
-          >
+        <Link href={`/daily-runs/${today}`} passHref>
+          <NavBarItem active={pathName.startsWith("/daily-runs")}>
             Todays run
           </NavBarItem>
         </Link>
-        <Link href="/orders">
+        <Link href="/orders" passHref>
           <NavBarItem active={pathName === "/orders"}>All orders</NavBarItem>
         </Link>
-        <Link href="/portfolio">
+        <Link href="/portfolio" passHref>
           <NavBarItem active={pathName === "/portfolio"}>Portfolio</NavBarItem>
         </Link>
       </LinksContainer>
